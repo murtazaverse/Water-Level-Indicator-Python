0e094507cd72034034816a3360de806748e2863d
Murtaza Zaidi <49615541+syedmurtazazaidi@users.noreply.github.com>
2022-02-15 17:20:23 +0500

    Update write.py

--- write.py
+++ write.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-
 import time
 import RPi.GPIO as GPIO
 def ping():
@@ -9,8 +8,8 @@ def ping():
     trig_2 = 26
     echo_2 = 19
     while True:
-    # Echo
-        GPIO.setup(trig_2,GPIO.OUT) # Trigger
+     # Echo
+        GPIO.setup(trig_2,GPIO.OUT)  # Trigger
         GPIO.setup(echo_2,GPIO.IN)
         GPIO.output(trig_2, False)
         time.sleep(1.5)
@@ -19,28 +18,29 @@ def ping():
         GPIO.output(trig_2, False)
         start1 = time.time()
         while GPIO.input(echo_2)==0:
-        start1 = time.time()
+            start1 = time.time()
         while GPIO.input(echo_2)==1:
-        stop1= time.time()
+            stop1= time.time()
         elapsed1 = stop1-start1
         distancet1 = (elapsed1*34300)-0.5
         distance1 = distancet1/2
         elapsed1=round(elapsed1,6)
+    
         distance1 = round(distance1,0)
         y=str(distance1)
         x=str(elapsed1)
-        if distance1&lt;5:
-        print(&quot;Tank Is Full&quot;)
-        elif distance1&gt;8 and distance1&lt;12:
-        print(&quot;Tank Is Approximately Half&quot;)
-        elif distance1&gt;18 and distance1&lt;21:
-        print(&quot;Tank Is Empty&quot;)
-        z=x+&quot;,&quot;+y+&quot;\n&quot;
+        if distance1<5:
+            print("Tank Is Full")
+        elif distance1>8 and distance1<12:
+            print("Tank Is Approximately Half")
+        elif distance1>18 and distance1<21:
+            print("Tank Is Empty")
+        z=x+","+y+"\n"
         print(z)
-        file=&#39;text.txt&#39;
-        with open(file,&#39;a&#39;) as filetowrite:
-        filetowrite.write(z)
-        filetowrite.close()
+        file='text.txt'
+        with open(file,'a') as filetowrite:
+            filetowrite.write(z)
+            filetowrite.close()
     GPIO.cleanup()
     return distance1
 while True:
