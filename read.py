22eea358fe14eb3211ebc5d6a58540534b6f12a9
Murtaza Zaidi <49615541+syedmurtazazaidi@users.noreply.github.com>
2022-02-15 17:20:51 +0500

    Update read.py

--- read.py
+++ read.py
@@ -4,28 +4,28 @@ import numpy as np
 import time
 
 fig = plt.figure()
+
 def animate(i):
-    pullData = open(&quot;text.txt&quot;,&quot;r&quot;).read()
-    dataArray = pullData.split(&#39;\n&#39;)
-    xar = []
-    yar = []
-    for eachLine in dataArray:
-        if len(eachLine)&gt;1:
-            x,y = eachLine.split(&#39;,&#39;)
-            xar.append(float(x))
-            yar.append(float(y))
-    
-    mur=plt.plot(xar,yar)
-    
-    y_corr=np.correlate(yar,yar,mode=&#39;full&#39;)
-    
-    plt.subplot(2,1,1)
-    plt.stem(xar,yar)
-    plt.title(&#39;Actual Graph&#39;)
-    
-    plt.subplot(2,1,2)
-    plt.stem(y_corr,maxlags=11)
-    plt.title(&#39;Correlated Distance&#39;)
-ani = animation.FuncAnimation(fig, animate,
-interval=1000)
+    pullData = open("text.txt","r").read()
+    dataArray = pullData.split('\n')
+    xar = []
+    yar = []
+    for eachLine in dataArray:
+        if len(eachLine)>1:
+            x,y = eachLine.split(',')
+            xar.append(float(x))
+            yar.append(float(y))
+    
+    mur=plt.plot(xar,yar)
+    
+    y_corr=np.correlate(yar,yar,mode='full')
+    
+    plt.subplot(2,1,1)
+    plt.stem(xar,yar)
+    plt.title('Actual Graph')
+    
+    plt.subplot(2,1,2)
+    plt.stem(y_corr,maxlags=11)
+    plt.title('Correlated Distance')
+ani = animation.FuncAnimation(fig, animate, interval=1000)
 plt.show()
